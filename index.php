0e1afddcd37eb4f119cca0b221ae67a48e1e84aa
cuateisaac <68478828+cuateisaac@users.noreply.github.com>
2020-07-18 11:36:02 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,121 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="assets/styles.css">
+        <title>Hello, world!</title>
+    </head>
+    <body class="bg-secondary body nav-text container text-warning body-img">
+        <div class="container">
+            <!-- Image and text -->
+            <div class="py-1 d-flex align-items-center nav-text text-right" style="background-color: black; ">
+                <img src="assets/img/logo.jpeg" width="150" height="150" class="d-inline-block align-top" alt="" loading="lazy">
+
+                <ul class="nav text-warning justify-content-end">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-warning font-weight-bold">Fandom News</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-warning" href="#">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-warning" href="#">Juegos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-warning" href="#">Tecnologia</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-warning" href="#">Tienda</a>
+                    </li>
+                    <form class="form-inline my-2 my-lg-0 ml-5">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-warning my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </ul>
+            </div>
+        </div>>
+
+        <!-- carrucel -->
+        <div class="container">
+            <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="assets/img/N.jpg" height="450" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block text-warning">
+                            <div class="div-bg">
+                                <h5>Este es el titulo</h5>
+                                <p>Esta la descripcion</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="assets/img/logo.jpeg" height="450" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block text-warning">
+                            <div class="div-bg">
+                                <h5>Second slide label</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="assets/img/logo.jpeg" height="450" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block text-warning">
+                            <div class="div-bg">
+                                <h5>Third slide label</h5>
+                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+        <!-- cards -->
+        
+        <div class="row py-5">
+                <div class="col-md-4 div-bg">
+                    <h3>Titulo 1</h3>
+                    <hr>
+                    <img src="assets/img/N.jpg" class="img-fluid rounded">
+                    <p>Globalización y tecnología son dos conceptos íntimamente ligados. No podemos entender la globalización solamente 
+                        como un proceso económico</p>
+                </div>
+                <div class="col-md-4 div-bg">
+                    <h3>Titulo 2</h3>
+                    <hr>
+                    <img src="assets/img/N.jpg" class="img-fluid rounded">
+                    <p>Globalización y tecnología son dos conceptos íntimamente ligados. No podemos entender la globalización solamente 
+                        como un proceso económico</p>
+                </div>
+                <div class="col-md-4 div-bg">
+                    <h3>Titulo 3</h3>
+                    <hr>
+                    <img src="assets/img/N.jpg" class="img-fluid rounded">
+                    <p>Globalización y tecnología son dos conceptos íntimamente ligados. No podemos entender la globalización solamente 
+                        como un proceso económico</p>
+                </div>
+        </div>
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    </body>
+</html>
